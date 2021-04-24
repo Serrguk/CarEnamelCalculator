51745a7126b41a6ef4abca8c31348c0e3c6bce76
Milyukov Sergey <serrguk@gmail.om>
2021-04-25 00:18:06 +0700

    Main modified, Jframe added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-     CalculationMethod.calculate();
+        JFrame frame = getFrame();
+
+    }
+    static class Component extends JComponent {
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g;
+            g2.drawString("Чикибряк", 20, 20);
+        }
+    }
+
+    static JFrame getFrame() {
+        JFrame frame = new JFrame("Расчёт объёма");
+        frame.setVisible(true);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        frame.setBounds(dimension.width / 2 - 300, dimension.height / 2 - 200, 600, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        return frame;
     }
 }
